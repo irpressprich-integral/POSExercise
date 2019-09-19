e263d5b46d03403bf6db8c5ec79e17057df54ef2
robertpressprich <robert.pressprich@integral.io>
2019-09-19 16:32:27 -0400

    we can now format any positive amount of US currency

--- src/test/test/formatCurrencyTest.java
+++ src/test/test/formatCurrencyTest.java
@@ -2,16 +2,15 @@ package test;
 
 import static junit.framework.TestCase.assertEquals;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import org.junit.Test;
 
 public class formatCurrencyTest {
 
     private static String formatCurrency(long priceInCents) {
-        if (priceInCents == 0) {
-            return "$0.00";
-        } else {
-            return "$1.00";
-        }
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+        return n.format(priceInCents / 100.0);
     }
 
     @Test
@@ -22,5 +21,40 @@ public class formatCurrencyTest {
     @Test
     public void hundredCents() {
         assertEquals("$1.00", formatCurrency(100));
+    }
+
+    @Test
+    public void thousandCents() {
+        assertEquals("$10.00", formatCurrency(1000));
+    }
+
+    @Test
+    public void fiveCents() {
+        assertEquals("$0.05", formatCurrency(5));
+    }
+
+    @Test
+    public void tenCents() {
+        assertEquals("$10.00", formatCurrency(1000));
+    }
+
+    @Test
+    public void twoSeventyFiveCents() {
+        assertEquals("$2.75", formatCurrency(275));
+    }
+
+    @Test
+    public void oneCent() {
+        assertEquals("$0.01", formatCurrency(1));
+    }
+
+    @Test
+    public void oneHundredThousandCents() {
+        assertEquals("$1,000.00", formatCurrency(100000));
+    }
+
+    @Test
+    public void oneHundredMillonCent() {
+        assertEquals("$1,000,000.00", formatCurrency(100000000));
     }
 }
